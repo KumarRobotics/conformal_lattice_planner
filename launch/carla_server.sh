e766a3536d6c8e0edbe40fd0da6b826fb86a0080
Ke Sun <sunke.polyu@gmail.com>
2019-08-28 10:11:51 -0400

    Sleep between different configs to prevent timeout

--- launch/carla_server.sh
+++ launch/carla_server.sh
@@ -17,9 +17,13 @@ echo "Change map to Town04."
 cd PythonAPI/util
 python config.py --map Town04
 
+sleep 3s
+
 # Disable rendering
 echo "Disable rendering of the CARLA server (GPU related data won't work)."
 python config.py --no-rendering
+
+sleep 3s
 
 # Set the simulation step
 echo "Simulation step is set to 0.05s (20Hz)."
