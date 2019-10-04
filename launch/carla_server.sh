534a6c761404e9dd21d1585be4ebb0f65eafd83e
Ke Sun <sunke.polyu@gmail.com>
2019-10-03 22:53:39 -0400

    Remove the commented line

--- launch/carla_server.sh
+++ launch/carla_server.sh
@@ -2,7 +2,6 @@
 trap "trap - TERM && kill -- -$$" INT TERM EXIT
 
 CWD=`pwd`
-#cd ~/RosWorkSpace/self_driving_ws/src/carla/Dist/CARLA_Shipping_0.9.6-16-gd1d9174a-dirty/LinuxNoEditor
 cd ${Carla_DIST}
 
 # Start Carla server without display
